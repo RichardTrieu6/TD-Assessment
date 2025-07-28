18b21e22c96c334921668ddace404060ec88a9de
Richard <128167917+RichardTrieu6@users.noreply.github.com>
2025-07-28 16:57:40 -0400

    Update TDQ1.py

--- TDQ1.py
+++ TDQ1.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import json
+import re
 
 class Sentiment (BaseModel):
     publish_date: str
@@ -15,11 +16,10 @@ class Sentiment (BaseModel):
 
 df = pd.read_csv('TDS Global FX Assessment - Headlines.csv')
 
-# Sample headline and date for testing
-date = "2025-05-27 09:44:46"
-headline = "Nvidia Valued More Than Germany Points to Mispricing"
 
-prompt = f"""
+# Get llama3.2 response from prompt 
+def llama(headline, date):
+    prompt = f"""
         You are an expert financial analyst and trader. 
         Given the headline: {headline}, with date: {date}, 
         your task is to provide only a JSON output of the following structure:
@@ -30,34 +30,52 @@ prompt = f"""
         sentiment: str (Positive, Neutral, or Negative)
         
         Headlines unrelated to finance should have neutral sentiment, Other for asset class, and N/A for country.
+        The country label for headlines about a foreign currency should have the associated country for that currency.
+        For example, if the headline is about CAD, the country should be Canada
 
         For example:
         Headline: IBM PLANS TO INVEST $150B IN AMERICA OVER NEXT FIVE YEARS
         Date: 2025-04-28 08:31:39
         Output: 
         {{
-            publish_date: "2025-04-28 08:31:39",
-            context: "IBM PLANS TO INVEST $150B IN AMERICA OVER NEXT FIVE YEARS",
-            assetclass: "Equities",
-            country: "US",
-            sentiment: "Positive"
+            "publish_date": "2025-04-28 08:31:39",
+            "context": "IBM PLANS TO INVEST $150B IN AMERICA OVER NEXT FIVE YEARS",
+            "assetclass": "Equities",
+            "country": "US",
+            "sentiment": "Positive"
         }}
+
+        Your output must be only be valid JSON and nothing else. Include both opening and closing brackets and commas.
         """
+    
+    try:
+        response = chat(
+            model='llama3.2',
+            messages=[{'role': 'user', 'content': prompt}],
+            format='json', 
+            options={'temperature': 0}  
+        )
+        
+        # Clean LLM Output
+        content = response['message']['content'].strip()
+        data = json.loads(content)
+        return data
+        
+    except Exception as e:
+        return None
 
-# Get llama3.2 response from prompt 
-response: ChatResponse = chat(model='llama3.2', messages=[
-    {
-        'role': 'user',
-        'content': prompt
-    }
-])
 
-print(response['message']['content'])
 
-data = json.loads(response['message']['content'])
-
-sentiment = Sentiment(**data)
-print(sentiment)
 
 # Split data 
 train_df, val_df = train_test_split(df, test_size=0.2, random_state=30)
+results = []
+for i in range(10):
+    date = df.iloc[i]['date']
+    headline = df.iloc[i]['headline']
+    result = llama(headline=headline,date=date)
+    results.append(result)
+    print(i)
+    
+results_df=pd.DataFrame.from_dict(results)
+results_df.to_json('sentiment_results.csv', orient='records',lines=True)
